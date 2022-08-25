1888f9e3a4c1a54dc6330aee6c44466dd49342ec
vikalp1999 <shandilyavikalp@gmail.com>
2022-08-25 17:42:04 +0530

    added project

---
+++ login/login.js
@@ -0,0 +1,25 @@
+let data=JSON.parse(localStorage.getItem("shapeLinksignupData"))||[];
+class User{
+  constructor(){
+
+  }
+  login(loginusername,loginpassword){
+    data.forEach(function(el){
+      if(el.username==loginusername&&el.password==loginpassword){
+        alert("sucessfull login");
+        console.log(el)
+        localStorage.setItem("logindata",JSON.stringify(el))
+      }else{
+        alert("Details are not correct!")
+      }
+    })
+  }
+}
+
+
+let login=()=>{
+  let loginusername=document.getElementById("loginUsername").value;
+  let loginpassword=document.getElementById("loginPassword").value;
+  let user=new User();
+  user.login(loginusername,loginpassword) 
+}
